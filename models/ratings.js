36078016f2f0c830d38ac5c9f992bebd4407632d
Nassima Bouziane <nassimabouziane@Nassimas-MacBook-Air.local>
2019-02-17 13:38:55 -0800

    add const to school schema and ratings schema

--- models/ratings.js
+++ models/ratings.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
   // import School and User 
   School = require('./school');
   User = require('./user')
--- models/school.js
+++ models/school.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 
 const SchoolSchema = new Schema({
